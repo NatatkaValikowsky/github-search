148cf3f924caa0581e041d9ba50df628534f601c
Valikova Natalya <valikowa.natalya@yandex.ru>
2020-11-12 22:19:57 +0900

    promises

--- script.js
+++ script.js
@@ -10,41 +10,43 @@ async function getData(e){
     resultsBlock.innerHTML = '';
     if(searchField.value.length >= startSearchCount){
         resultsBlock.innerHTML = 'Ищем...';
-        let response = await fetch(`https://api.github.com/search/repositories?q=${searchField.value}`);
-        if(response.ok){
-            const json = await response.json();
-            let limit = searchLimit < json.total_count ? searchLimit : json.total_count;
-            resultsBlock.innerHTML = '';
-            for(let i = 0; i < limit; i += 1) {
-                let clone = document.createElement('li');
-                clone.classList.add('search-result');
-                clone.innerHTML = json.items[i].name;
-                clone.setAttribute('dataSrc', JSON.stringify({
-                    name: json.items[i].name,
-                    owner: json.items[i].owner.login,
-                    stars: json.items[i].stargazers_count
-                }));
-                resultsBlock.append(clone);
-            }
-        } else{
-            resultsBlock.innerHTML = 'Возникла ошибка';
-        }
-
-        document.querySelectorAll('.search-result').forEach(function (el) {
-            el.addEventListener('click', function (e) {
-                let clone = document.importNode(selectedTemplate.content, true);
-                const attributes = JSON.parse(el.getAttribute('dataSrc'));
-                clone.querySelector('.repo-name').innerHTML = `Name: ${attributes.name}`;
-                clone.querySelector('.repo-owner').innerHTML = `Owner: ${attributes.owner}`;
-                clone.querySelector('.repo-stars').innerHTML = `Stars: ${attributes.stars}`;
-                clone.querySelector('.close-btn').addEventListener('click', function (e) {
-                    this.closest('.selected-repo').remove();
-                });
-                selectedBlock.append(clone);
-                searchField.value = '';
+        fetch(`https://api.github.com/search/repositories?q=${searchField.value}`)
+            .then(response => response.json())
+            .then(json => {
+                let limit = searchLimit < json.total_count ? searchLimit : json.total_count;
                 resultsBlock.innerHTML = '';
+                for(let i = 0; i < limit; i += 1) {
+                    let clone = document.createElement('li');
+                    clone.classList.add('search-result');
+                    clone.innerHTML = json.items[i].name;
+                    clone.setAttribute('dataSrc', JSON.stringify({
+                        name: json.items[i].name,
+                        owner: json.items[i].owner.login,
+                        stars: json.items[i].stargazers_count
+                    }));
+                    resultsBlock.append(clone);
+                }
+
+                document.querySelectorAll('.search-result').forEach(function (el) {
+                    el.addEventListener('click', function (e) {
+                        console.log('click');
+                        let clone = document.importNode(selectedTemplate.content, true);
+                        const attributes = JSON.parse(el.getAttribute('dataSrc'));
+                        clone.querySelector('.repo-name').innerHTML = `Name: ${attributes.name}`;
+                        clone.querySelector('.repo-owner').innerHTML = `Owner: ${attributes.owner}`;
+                        clone.querySelector('.repo-stars').innerHTML = `Stars: ${attributes.stars}`;
+                        clone.querySelector('.close-btn').addEventListener('click', function (e) {
+                            this.closest('.selected-repo').remove();
+                        });
+                        selectedBlock.append(clone);
+                        searchField.value = '';
+                        resultsBlock.innerHTML = '';
+                    });
+                });
+            })
+            .catch(error => {
+                resultsBlock.innerHTML = 'Возникла ошибка';
             });
-        });
     }
 }
 
